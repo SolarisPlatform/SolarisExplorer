19c4270c2cac1096ea14224be2397470efbcc830
Kees de Wit <keesdewit@keesdewit.nl>
2019-09-13 15:09:19 +0200

    Show correct balance figures and transaction count for a address

--- Solaris.BlockExplorer.Database/storedprocedures/StoredProcedures/Read/GetAddress.sql
+++ Solaris.BlockExplorer.Database/storedprocedures/StoredProcedures/Read/GetAddress.sql
@@ -8,50 +8,26 @@ DECLARE @SentCount BIGINT
 DECLARE @Mined DECIMAL(28, 8)
 DECLARE @MinedCount BIGINT
 
-SELECT
-	@ReceivedCount = COUNT(DISTINCT BlockTransactions.Id),
-	@Received = SUM(BlockTransactionOutputs.Value)
-FROM
-	tables.Blocks
-LEFT JOIN
-	tables.Transactions BlockTransactions
-ON
-	BlockTransactions.BlockId = tables.Blocks.Id
-LEFT JOIN
-	tables.Inputs BlockTransactionInputs
-ON
-	BlockTransactionInputs.TransactionId = BlockTransactions.Id
-LEFT JOIN
-	tables.Outputs BlockTransactionOutputs
-ON
-	BlockTransactionOutputs.TransactionId = BlockTransactions.Id
-LEFT JOIN
-	tables.Outputs BlockTransactionInputOutput
-ON
-	BlockTransactionInputOutput.Id = BlockTransactionInputs.OutputId
-WHERE
-	BlockTransactionOutputs.[Addresses] LIKE '%' + @PublicKey + '%'
-AND NOT
-	BlockTransactionInputOutput.[Addresses] = BlockTransactionOutputs.[Addresses]
-AND NOT
-	BlockTransactionInputs.OutputId IS NULL
-
-
-SELECT
-	@Sent = ISNULL(SUM(tables.Outputs.[Value]), 0),
-	@SentCount = COUNT(DISTINCT InputTransactions.Id)
-FROM
+SELECT 
+	@Sent = SUM(tables.Outputs.[Value]),
+	@SentCount = COUNT(DISTINCT tables.Inputs.TransactionId)
+FROM 
 	tables.Inputs
-LEFT JOIN
-	tables.Transactions InputTransactions
-ON
-	InputTransactions.Id = tables.Inputs.TransactionId
-LEFT JOIN
-	tables.Outputs
-ON
+INNER JOIN 
+	tables.Outputs 
+ON 
 	tables.Outputs.Id = tables.Inputs.OutputId
-WHERE
-	tables.Outputs.[Addresses] LIKE '%' + @PublicKey + '%'
+WHERE 
+	tables.Outputs.Addresses like '%' + @PublicKey + '%' 
+
+SELECT 
+	@Received = SUM(tables.Outputs.[Value]),
+	@ReceivedCount = COUNT(DISTINCT tables.Outputs.TransactionId)
+FROM 
+	tables.Outputs
+WHERE 
+	tables.Outputs.Addresses like '%' + @PublicKey + '%' 
+
 
 SELECT
 	@MinedCount = COUNT(DISTINCT BlockTransactions.Id),
