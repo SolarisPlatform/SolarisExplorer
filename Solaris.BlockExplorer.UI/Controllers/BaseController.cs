30c6643469bf8a76d853424c147359dda7bcda0a
Andrej Safranovic <viscolex@hotmail.com>
2019-09-13 12:31:20 +0100

    Make Social channels in footer configurable through appsettings

--- Solaris.BlockExplorer.UI/Controllers/BaseController.cs
+++ Solaris.BlockExplorer.UI/Controllers/BaseController.cs
@@ -19,8 +19,26 @@ namespace Solaris.BlockExplorer.UI.Controllers
             var name = section.GetValue<string>("Name");
             var ticker = section.GetValue<string>("Ticker");
 
+            var socials = _configuration.GetSection("Socials");
+            var website = socials.GetValue<string>("Website");
+            var twitter = socials.GetValue<string>("Twitter");
+            var telegram = socials.GetValue<string>("Telegram");
+            var discord = socials.GetValue<string>("Discord");
+            var reddit = socials.GetValue<string>("Reddit");
+            var bitcointalk = socials.GetValue<string>("BitcoinTalk");
+            var medium = socials.GetValue<string>("Medium");
+            var github = socials.GetValue<string>("GitHub");
+
             ViewBag.Name = name;
             ViewBag.Ticker = ticker;
+            ViewBag.Website = website;
+            ViewBag.Twitter = twitter;
+            ViewBag.Telegram = telegram;
+            ViewBag.Discord = discord;
+            ViewBag.Reddit = reddit;
+            ViewBag.BitcoinTalk = bitcointalk;
+            ViewBag.Medium = medium;
+            ViewBag.GitHub = github;
             base.OnActionExecuting(filterContext);
         }
     }
