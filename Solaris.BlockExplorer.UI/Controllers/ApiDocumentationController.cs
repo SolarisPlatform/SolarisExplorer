dd5cf873361d5c615af6ac1a223a6352fcbb8c10
Kees de Wit <keesdewit@keesdewit.nl>
2019-09-23 20:53:20 +0200

    Update project files

---
+++ Solaris.BlockExplorer.UI/Controllers/ApiDocumentationController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Solaris.BlockExplorer.UI.Controllers
+{
+    public class ApiDocumentationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
